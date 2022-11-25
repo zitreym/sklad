b70eaf4b103f7de85b02a28947b590c6693d95a6
BaranovD <baranovd@ro.ru>
2022-11-26 00:01:39 +0300

    Изменены статические адреса2

--- qr/print.php
+++ qr/print.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 include_once("class.php");
 ob_end_clean();
 Header('Content-type: image/png');
-$text = 'http://node2.treym.ru/detail/?id=';
+$text = 'https://node2.treym.ru/detail/?id=';
 $text = $text.$id;
 echo base64_decode((new QrCode($text))->outputCustomPng(300));
 ?>
